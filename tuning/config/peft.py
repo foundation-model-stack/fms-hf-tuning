aea4ca2bd45a9905f5c33cd46350ff756e5b965c
Linsong Chu <lchu@us.ibm.com>
2023-12-05 14:39:18 -0500

    make overall configs aligned

--- tuning/config/peft.py
+++ tuning/config/peft.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import List
 
+
 @dataclass
 class lora_config:
     r: int = 16
@@ -10,10 +11,11 @@ class lora_config:
     task_type: str = "CAUSAL_LM"
     lora_dropout: float = 0.05
 
+
 @dataclass
 class prompt_tuning_config:
     task_type: str = "CAUSAL_LM"
     prompt_tuning_init: str = "TEXT"
-    num_virtual_tokens: int = 8,
-    prompt_tuning_init_text = "Classify if the tweet is a complaint or not:",
-    tokenizer_name_or_path = "Llama-2-7b-hf"
+    num_virtual_tokens: int = 8
+    prompt_tuning_init_text: str = "Classify if the tweet is a complaint or not:"
+    tokenizer_name_or_path: str = "llama-7b-hf"
