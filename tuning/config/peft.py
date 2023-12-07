6b6af8c914643b518dc4577526f7deef71136045
Linsong Chu <lchu@us.ibm.com>
2023-12-07 11:11:51 -0500

    make default lora r to 8

--- tuning/config/peft.py
+++ tuning/config/peft.py
@@ -4,7 +4,7 @@ from typing import List
 
 @dataclass
 class lora_config:
-    r: int = 16
+    r: int = 8
     lora_alpha: int = 32
     target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj"])
     bias = "none"
