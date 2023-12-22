5bfbab2bb09e305c064df321fd81c7aba44d4e94
Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
2023-12-22 15:28:08 -0700

    allows disable flash attn and torch dtype param
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>

---
+++ tuning/utils/data_type_utils.py
@@ -0,0 +1,43 @@
+# Standard
+from typing import Union
+
+# Third Party
+from transformers.utils import logging
+import torch
+
+logger = logging.get_logger("data_utils")
+
+def str_to_torch_dtype(dtype_str: str) -> torch.dtype:
+    """Given a string representation of a Torch data type, convert it to the actual torch dtype.
+
+    Args:
+        dtype_str: String representation of Torch dtype to be used; this should be an attr
+        of the torch library whose value is a dtype.
+
+    Returns:
+        torch.dtype
+            Data type of the Torch class being used.
+    """
+    dt = getattr(torch, dtype_str, None)
+    if not isinstance(dt, torch.dtype):
+        logger.error(" ValueError: Unrecognized data type")
+    return dt
+
+
+def get_torch_dtype(dtype: Union[str, torch.dtype]) -> torch.dtype:
+    """Get the Torch data type to be used for interacting with a model.
+
+    Args:
+        dtype: Union[str, torch.dtype]
+            If dtype is a torch.dtype, returns it; if it's a string, grab it from the Torch lib.
+
+    Returns:
+        torch.dtype
+            Torch data type to be used.
+    """
+    # If a Torch dtype is passed, nothing to do
+    if isinstance(dtype, torch.dtype):
+        return dtype
+    # TODO - If None/empty str was provided, read it from model config?
+    # Otherwise convert it from a string
+    return str_to_torch_dtype(dtype)
