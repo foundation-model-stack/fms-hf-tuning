9c2174aa6f2dc9bf27caf00f6d0fa7a44e11adc0
Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
2024-01-15 19:17:19 -0700

    experimental:get format for seq_seq models
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -23,10 +23,10 @@ from peft.utils.other import fsdp_auto_wrap_policy
 class PeftSavingCallback(TrainerCallback):
     def on_save(self, args, state, control, **kwargs):
         checkpoint_path = os.path.join(args.output_dir, f"checkpoint-{state.global_step}")
-        kwargs["model"].save_pretrained(checkpoint_path)
+        kwargs["model"].save_pretrained(checkpoint_path, safe_serialization=False)
 
-        if "pytorch_model.bin" in os.listdir(checkpoint_path):
-            os.remove(os.path.join(checkpoint_path, "pytorch_model.bin"))
+        # if "pytorch_model.bin" in os.listdir(checkpoint_path):
+        #     os.remove(os.path.join(checkpoint_path, "pytorch_model.bin"))
 
 
 def train(
@@ -61,8 +61,8 @@ def train(
         train_args.fsdp = ""
         train_args.fsdp_config = {'xla':False}
 
-    task_type = "CAUSAL_LM"
-    model = transformers.AutoModelForCausalLM.from_pretrained(
+    task_type = "SEQ_2_SEQ_LM"
+    model = transformers.AutoModelForSeq2SeqLM.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=train_args.cache_dir,
         torch_dtype=get_torch_dtype(model_args.torch_dtype),
@@ -81,41 +81,41 @@ def train(
         padding_side="right",
         use_fast = True
     )
-    if isinstance(tokenizer, LlamaTokenizer) or isinstance(tokenizer, LlamaTokenizerFast):
-        tokenizer.add_special_tokens({
-            "bos_token": "<s>",
-            "eos_token": "</s>",
-            "unk_token": "<unk>",
-            "pad_token": "<pad>",
-        })
-    elif isinstance(tokenizer, GPTNeoXTokenizerFast):
-        tokenizer.add_special_tokens({
-            "pad_token": "<pad>",
-        })
+    # if isinstance(tokenizer, LlamaTokenizer) or isinstance(tokenizer, LlamaTokenizerFast):
+    #     tokenizer.add_special_tokens({
+    #         "bos_token": "<s>",
+    #         "eos_token": "</s>",
+    #         "unk_token": "<unk>",
+    #         "pad_token": "<pad>",
+    #     })
+    # elif isinstance(tokenizer, GPTNeoXTokenizerFast):
+    #     tokenizer.add_special_tokens({
+    #         "pad_token": "<pad>",
+    #     })
     
     model_max_length = tokenizer.model_max_length
     logger.info(f"Model max length {model_max_length}")
     
-    # TODO: we need to change this, perhaps follow what open instruct does?
-    special_tokens_dict = dict()
-    if tokenizer.pad_token is None:
-        logger.warning("PAD token set to default, missing in tokenizer")
-        special_tokens_dict["pad_token"] = configs.DEFAULT_PAD_TOKEN
-    if tokenizer.eos_token is None:
-        logger.warning("EOS token set to default, missing in tokenizer")
-        special_tokens_dict["eos_token"] = configs.DEFAULT_EOS_TOKEN
-    if tokenizer.bos_token is None:
-        logger.warning("BOS token set to default, missing in tokenizer")
-        special_tokens_dict["bos_token"] = configs.DEFAULT_BOS_TOKEN
-    if tokenizer.unk_token is None:
-        logger.warning("UNK token set to default, missing in tokenizer")
-        special_tokens_dict["unk_token"] = configs.DEFAULT_UNK_TOKEN
+    # # TODO: we need to change this, perhaps follow what open instruct does?
+    # special_tokens_dict = dict()
+    # if tokenizer.pad_token is None:
+    #     logger.warning("PAD token set to default, missing in tokenizer")
+    #     special_tokens_dict["pad_token"] = configs.DEFAULT_PAD_TOKEN
+    # if tokenizer.eos_token is None:
+    #     logger.warning("EOS token set to default, missing in tokenizer")
+    #     special_tokens_dict["eos_token"] = configs.DEFAULT_EOS_TOKEN
+    # if tokenizer.bos_token is None:
+    #     logger.warning("BOS token set to default, missing in tokenizer")
+    #     special_tokens_dict["bos_token"] = configs.DEFAULT_BOS_TOKEN
+    # if tokenizer.unk_token is None:
+    #     logger.warning("UNK token set to default, missing in tokenizer")
+    #     special_tokens_dict["unk_token"] = configs.DEFAULT_UNK_TOKEN
 
-    tokenizer_data_utils.tokenizer_and_embedding_resize(
-        special_tokens_dict=special_tokens_dict,
-        tokenizer=tokenizer,
-        model=model,
-    )
+    # tokenizer_data_utils.tokenizer_and_embedding_resize(
+    #     special_tokens_dict=special_tokens_dict,
+    #     tokenizer=tokenizer,
+    #     model=model,
+    # )
     
     # load the data by parsing JSON
     json_dataset = datasets.load_dataset('json', data_files=data_args.data_path)
@@ -124,30 +124,28 @@ def train(
     aim_callback = get_aimstack_callback()
     callbacks=[aim_callback,PeftSavingCallback()]
 
-    if train_args.packing:
-        logger.info("Packing is set to True")
-        data_collator = None
-        packing = True
-    else:
-        logger.info("Packing is set to False")
-        if data_args.response_template is None:
-            logger.error("Error, response template is None, needs to be set for training")
-            exit(-1)
+    # if train_args.packing:
+    #     logger.info("Packing is set to True")
+    #     data_collator = None
+    #     packing = True
+    # else:
+    #     logger.info("Packing is set to False")
+    #     if data_args.response_template is None:
+    #         logger.error("Error, response template is None, needs to be set for training")
+    #         exit(-1)
         
-        if data_args.dataset_text_field is None:
-            logger.error("Error, dataset_text_field is None, needs to be set for training")
-            exit(-1)
+    #     if data_args.dataset_text_field is None:
+    #         logger.error("Error, dataset_text_field is None, needs to be set for training")
+    #         exit(-1)
 
-        response_template_ids = tokenizer.encode(data_args.response_template, add_special_tokens=False)[2:]
-        data_collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer, ignore_index=configs.IGNORE_INDEX)
-        packing = False
+    #     response_template_ids = tokenizer.encode(data_args.response_template, add_special_tokens=False)[2:]
+    #     data_collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer, ignore_index=configs.IGNORE_INDEX)
+    #     packing = False
 
     trainer = SFTTrainer(
         model=model,
         tokenizer=tokenizer,
         train_dataset=json_dataset['train'],
-        packing=packing,
-        data_collator=data_collator,
         dataset_text_field=data_args.dataset_text_field,
         args=train_args,
         max_seq_length=model_max_length,
