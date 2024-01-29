23c4bb2d17b9a1f37b37164955e0d50c408a4e10
Alex-Brooks <Alex.Brooks@ibm.com>
2024-01-29 03:10:22 -0600

    Add adapter config overrides to inference script

--- tuning/tuned_model.py
+++ tuning/tuned_model.py
@@ -5,6 +5,8 @@ import argparse
 from transformers import AutoTokenizer
 from peft import AutoPeftModelForCausalLM
 
+from tuning.utils import AdapterConfigPatcher
+
 class TunedCausalLM:
     def __init__(self, model, tokenizer):
         self.peft_model = model
@@ -19,14 +21,16 @@ class TunedCausalLM:
         downloaded from HF hub, or a local path on disk, the latter of which we must be careful
         with when using a model that was written on a different device.
         """
-        if base_model_name_or_path is not None:
-            raise NotImplementedError("WIP: override support not yet implemented")
+        overrides = {"base_model_name_or_path": base_model_name_or_path} if base_model_name_or_path is not None else {}
         tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-        try:
-            peft_model = AutoPeftModelForCausalLM.from_pretrained(checkpoint_path)
-        except OSError as e:
-            print("Failed to initialize checkpoint model!")
-            raise e
+        # Apply the configs to the adapter config of this model; if no overrides
+        # are provided, then the context manager doesn't have any effect.
+        with AdapterConfigPatcher(checkpoint_path, overrides):
+            try:
+                peft_model = AutoPeftModelForCausalLM.from_pretrained(checkpoint_path)
+            except OSError as e:
+                print("Failed to initialize checkpoint model!")
+                raise e
         return cls(peft_model, tokenizer)
 
     def run(self, text: str) -> str:
