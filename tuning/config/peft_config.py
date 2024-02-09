d2b355daadf060a11ec5e52b83ab6930ae99613d
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2024-02-09 10:30:10 +0000

    fix: target_modules must be a List[] for HfArgumentParser to parse more than 1 command line value
    For more information see
    
    https://github.com/huggingface/transformers/blob/2749e479f30ab13235b0b9b4a6bbcf4c3b29a081/src/transformers/hf_argparser.py#L206-L208
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- tuning/config/peft_config.py
+++ tuning/config/peft_config.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass, field
-from typing import List, Union, Optional
+from typing import List
 
 @dataclass
 class LoraConfig:
     r: int = 8
     lora_alpha: int = 32
-    target_modules: Optional[Union[List[str], str]] = field(default_factory=lambda: ["q_proj", "v_proj"], metadata={
+    target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj"], metadata={
         "help": "The names of the modules to apply LORA to. When using a string, LORA uses regex matching. When using "
             "a list of strings, LORA selects modules which either completely match or end with one of the strings. "
             "If the value is the string 'all-linear', then LORA selects all linear and Conv1D modules except for "
