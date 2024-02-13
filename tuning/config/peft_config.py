2f571d19b8620003c49f5844c078db6d31845f1b
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2024-02-13 09:01:37 +0000

    docs: update docstring of target_modules to reflect new code
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- tuning/config/peft_config.py
+++ tuning/config/peft_config.py
@@ -6,11 +6,9 @@ class LoraConfig:
     r: int = 8
     lora_alpha: int = 32
     target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj"], metadata={
-        "help": "The names of the modules to apply LORA to. When using a string, LORA uses regex matching. When using "
-            "a list of strings, LORA selects modules which either completely match or end with one of the strings. "
-            "If the value is the string 'all-linear', then LORA selects all linear and Conv1D modules except for "
-            "the output layer. If the value is none, then LORA chooses modules depending on the model architecture. "
-            "In that case, in that case LORA raises an exception prompting the user to manually select the modules."
+        "help": "The names of the modules to apply LORA to. LORA selects modules which either completely match or "
+        "end with one of the strings. If the value is [\"all-linear\"], then LORA selects all linear and Conv1D "
+        "modules except for the output layer."
     })
     bias = "none"
     lora_dropout: float = 0.05
