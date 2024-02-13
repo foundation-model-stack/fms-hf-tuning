5f727d9755df6ad4ef3e5e9c3167d69674f2b5fd
Martin Hickey <martin.hickey@ie.ibm.com>
2024-02-13 16:49:58 +0000

    Enable code formatting
    Adds a GitHub workflow so its called when commits pushed.
    Also, changes for running the formatter locally are committed
    so the workflow will run as expected.
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -1,36 +1,51 @@
-import os
+# Standard
 from typing import Optional, Union
+import os
 
+# Third Party
+from peft.utils.other import fsdp_auto_wrap_policy
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    GPT2Tokenizer,
+    GPTNeoXTokenizerFast,
+    LlamaTokenizer,
+    LlamaTokenizerFast,
+    TrainerCallback,
+)
+from transformers.utils import logging
+from trl import DataCollatorForCompletionOnlyLM, SFTTrainer
 import datasets
 import fire
-from peft.utils.other import fsdp_auto_wrap_policy
 import torch
 import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer, LlamaTokenizerFast, GPTNeoXTokenizerFast, GPT2Tokenizer
-from transformers.utils import logging
-from transformers import TrainerCallback
-from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+
+# Local
 from tuning.aim_loader import get_aimstack_callback
 from tuning.config import configs, peft_config
 from tuning.data import tokenizer_data_utils
 from tuning.utils.config_utils import get_hf_peft_config
 from tuning.utils.data_type_utils import get_torch_dtype
 
+
 class PeftSavingCallback(TrainerCallback):
     def on_save(self, args, state, control, **kwargs):
-        checkpoint_path = os.path.join(args.output_dir, f"checkpoint-{state.global_step}")
+        checkpoint_path = os.path.join(
+            args.output_dir, f"checkpoint-{state.global_step}"
+        )
         kwargs["model"].save_pretrained(checkpoint_path)
 
         if "pytorch_model.bin" in os.listdir(checkpoint_path):
             os.remove(os.path.join(checkpoint_path, "pytorch_model.bin"))
 
 
-
 def train(
     model_args: configs.ModelArguments,
     data_args: configs.DataArguments,
     train_args: configs.TrainingArguments,
-    peft_config: Optional[Union[peft_config.LoraConfig, peft_config.PromptTuningConfig]] = None,
+    peft_config: Optional[
+        Union[peft_config.LoraConfig, peft_config.PromptTuningConfig]
+    ] = None,
 ):
     """Call the SFTTrainer
 
@@ -48,15 +63,19 @@ def train(
     logger = logging.get_logger("sft_trainer")
 
     # Validate parameters
-    if (not isinstance(train_args.num_train_epochs, float)) or (train_args.num_train_epochs <= 0):
+    if (not isinstance(train_args.num_train_epochs, float)) or (
+        train_args.num_train_epochs <= 0
+    ):
         raise ValueError("num_train_epochs has to be an integer/float >= 1")
-    if (not isinstance(train_args.gradient_accumulation_steps , int)) or (train_args.gradient_accumulation_steps <= 0):
+    if (not isinstance(train_args.gradient_accumulation_steps, int)) or (
+        train_args.gradient_accumulation_steps <= 0
+    ):
         raise ValueError("gradient_accumulation_steps has to be an integer >= 1")
 
     # make sure to unset FSDP args when running on single gpu
     if not run_distributed:
         train_args.fsdp = ""
-        train_args.fsdp_config = {'xla':False}
+        train_args.fsdp_config = {"xla": False}
 
     task_type = "CAUSAL_LM"
     model = AutoModelForCausalLM.from_pretrained(
@@ -65,43 +84,53 @@ def train(
         torch_dtype=get_torch_dtype(model_args.torch_dtype),
         use_flash_attention_2=model_args.use_flash_attn,
     )
-    
+
     peft_config = get_hf_peft_config(task_type, peft_config)
 
     model.gradient_checkpointing_enable()
 
     # TODO: Move these to a config as well
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.model_name_or_path,
-        cache_dir=train_args.cache_dir,
-        use_fast = True
+        model_args.model_name_or_path, cache_dir=train_args.cache_dir, use_fast=True
     )
 
     # TODO: understand if we need to hardcode these here or just use defaults in model
-    if isinstance(tokenizer, LlamaTokenizer) or isinstance(tokenizer, LlamaTokenizerFast):
-        tokenizer.add_special_tokens({
-            "bos_token": "<s>",
-            "eos_token": "</s>",
-            "unk_token": "<unk>",
-            "pad_token": "<pad>",
-        })
-    elif isinstance(tokenizer, GPTNeoXTokenizerFast) or isinstance(tokenizer, GPT2Tokenizer):
-        tokenizer.add_special_tokens({
-            "pad_token": "<pad>",
-        })
+    if isinstance(tokenizer, LlamaTokenizer) or isinstance(
+        tokenizer, LlamaTokenizerFast
+    ):
+        tokenizer.add_special_tokens(
+            {
+                "bos_token": "<s>",
+                "eos_token": "</s>",
+                "unk_token": "<unk>",
+                "pad_token": "<pad>",
+            }
+        )
+    elif isinstance(tokenizer, GPTNeoXTokenizerFast) or isinstance(
+        tokenizer, GPT2Tokenizer
+    ):
+        tokenizer.add_special_tokens(
+            {
+                "pad_token": "<pad>",
+            }
+        )
 
     """TODO: near term - how response template ids are parsed out needs to be cleaned.
        The [2:] here applies if response template has \n prefix, it is needed to strip \n, otherwise template is not found.
        We will create issue to clean this out after we discuss data formats and collators we will support
     """
-    response_template_ids = tokenizer.encode(data_args.response_template, add_special_tokens=False)[2:]
-    # TODO: This is actually max_seq_length and not model_max_length. we should not override model_max_length 
+    response_template_ids = tokenizer.encode(
+        data_args.response_template, add_special_tokens=False
+    )[2:]
+    # TODO: This is actually max_seq_length and not model_max_length. we should not override model_max_length
     # as in current main. We need to change name of this parameter we expose to users.
     model_max_length = min(train_args.model_max_length, tokenizer.model_max_length)
     logger.info(f"Model max length {model_max_length}")
     if train_args.model_max_length > tokenizer.model_max_length:
-        logger.warning(f"model_max_length {train_args.model_max_length} exceeds tokenizer.model_max_length {tokenizer.model_max_length}, using tokenizer.model_max_length {tokenizer.model_max_length}")
-    
+        logger.warning(
+            f"model_max_length {train_args.model_max_length} exceeds tokenizer.model_max_length {tokenizer.model_max_length}, using tokenizer.model_max_length {tokenizer.model_max_length}"
+        )
+
     # TODO: we need to change this, perhaps follow what open instruct does?
     special_tokens_dict = dict()
     if tokenizer.pad_token is None:
@@ -124,26 +153,29 @@ def train(
         tokenizer=tokenizer,
         model=model,
     )
-    
+
     # load the data by parsing JSON
     # TODO: update arg from data_path to training_data_path since we also have validation_data_path
     data_files = {"train": data_args.data_path}
     if data_args.validation_data_path:
         data_files["validation"] = data_args.validation_data_path
 
-    format_dataset = lambda example : {f"{data_args.dataset_text_field}" : example[f"{data_args.dataset_text_field}"] + tokenizer.eos_token}
+    format_dataset = lambda example: {
+        f"{data_args.dataset_text_field}": example[f"{data_args.dataset_text_field}"]
+        + tokenizer.eos_token
+    }
 
-    json_dataset = datasets.load_dataset('json', data_files=data_files)
-    formatted_train_dataset = json_dataset['train'].map(format_dataset)
+    json_dataset = datasets.load_dataset("json", data_files=data_files)
+    formatted_train_dataset = json_dataset["train"].map(format_dataset)
     logger.info(f"Training dataset length is {len(formatted_train_dataset)}")
 
     formatted_validation_dataset = None
     if data_args.validation_data_path:
-        formatted_validation_dataset = json_dataset['validation'].map(format_dataset)
+        formatted_validation_dataset = json_dataset["validation"].map(format_dataset)
         logger.info(f"Validation dataset length is {len(formatted_validation_dataset)}")
 
     aim_callback = get_aimstack_callback()
-    callbacks=[aim_callback,PeftSavingCallback()]
+    callbacks = [aim_callback, PeftSavingCallback()]
 
     if train_args.packing:
         logger.info("Packing is set to True")
@@ -152,14 +184,22 @@ def train(
     else:
         logger.info("Packing is set to False")
         if data_args.response_template is None:
-            logger.error("Error, response template is None, needs to be set for training")
+            logger.error(
+                "Error, response template is None, needs to be set for training"
+            )
             exit(-1)
-        
+
         if data_args.dataset_text_field is None:
-            logger.error("Error, dataset_text_field is None, needs to be set for training")
+            logger.error(
+                "Error, dataset_text_field is None, needs to be set for training"
+            )
             exit(-1)
-        
-        data_collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer, ignore_index=configs.IGNORE_INDEX)
+
+        data_collator = DataCollatorForCompletionOnlyLM(
+            response_template_ids,
+            tokenizer=tokenizer,
+            ignore_index=configs.IGNORE_INDEX,
+        )
         packing = False
 
     trainer = SFTTrainer(
@@ -177,25 +217,45 @@ def train(
     )
 
     if run_distributed and peft_config is not None:
-        trainer.accelerator.state.fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(model)
+        trainer.accelerator.state.fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(
+            model
+        )
     trainer.train()
 
 
 def main(**kwargs):
-    parser = transformers.HfArgumentParser(dataclass_types=(configs.ModelArguments, 
-                                                            configs.DataArguments,
-                                                            configs.TrainingArguments,
-                                                            peft_config.LoraConfig,
-                                                            peft_config.PromptTuningConfig))
-    parser.add_argument('--peft_method', type=str.lower, choices=['pt', 'lora', None, 'none'], default="pt")
-    model_args, data_args, training_args, lora_config, prompt_tuning_config, peft_method, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
+    parser = transformers.HfArgumentParser(
+        dataclass_types=(
+            configs.ModelArguments,
+            configs.DataArguments,
+            configs.TrainingArguments,
+            peft_config.LoraConfig,
+            peft_config.PromptTuningConfig,
+        )
+    )
+    parser.add_argument(
+        "--peft_method",
+        type=str.lower,
+        choices=["pt", "lora", None, "none"],
+        default="pt",
+    )
+    (
+        model_args,
+        data_args,
+        training_args,
+        lora_config,
+        prompt_tuning_config,
+        peft_method,
+        _,
+    ) = parser.parse_args_into_dataclasses(return_remaining_strings=True)
     if peft_method.peft_method == "lora":
-        tune_config=lora_config
-    elif peft_method.peft_method =="pt":
-        tune_config=prompt_tuning_config
+        tune_config = lora_config
+    elif peft_method.peft_method == "pt":
+        tune_config = prompt_tuning_config
     else:
-        tune_config=None
+        tune_config = None
     train(model_args, data_args, training_args, tune_config)
+
 
 if __name__ == "__main__":
     fire.Fire(main)
