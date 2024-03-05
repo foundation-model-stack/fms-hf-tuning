e4927a2a53ae9122b9da5d0de027b838b1a29f10
Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>
2024-03-05 15:25:18 -0800

    Fix formatting
    Signed-off-by: Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,21 +4,39 @@ import transformers
 # Local
 from tuning.config import configs, peft_config
 
+
 def causal_lm_train_kwargs(train_kwargs):
     """Parse the kwargs for a valid train call to a Causal LM."""
     parser = transformers.HfArgumentParser(
-    dataclass_types=(
-        configs.ModelArguments,
-        configs.DataArguments,
-        configs.TrainingArguments,
-        peft_config.LoraConfig,
-        peft_config.PromptTuningConfig,
+        dataclass_types=(
+            configs.ModelArguments,
+            configs.DataArguments,
+            configs.TrainingArguments,
+            peft_config.LoraConfig,
+            peft_config.PromptTuningConfig,
         )
     )
-    model_args, data_args, training_args, lora_config, prompt_tuning_config = parser.parse_dict(train_kwargs, allow_extra_keys=True)
+    (
+        model_args,
+        data_args,
+        training_args,
+        lora_config,
+        prompt_tuning_config,
+    ) = parser.parse_dict(train_kwargs, allow_extra_keys=True)
 
     # TODO: target_modules doesn't get set probably due to the way dataclass handles
     # mutable defaults, needs investigation on better way to handle this
-    setattr(lora_config, "target_modules", lora_config.__dataclass_fields__.get("target_modules").default_factory())
-    
-    return model_args, data_args, training_args, lora_config if train_kwargs.get("peft_method")=="lora" else prompt_tuning_config
+    setattr(
+        lora_config,
+        "target_modules",
+        lora_config.__dataclass_fields__.get("target_modules").default_factory(),
+    )
+
+    return (
+        model_args,
+        data_args,
+        training_args,
+        lora_config
+        if train_kwargs.get("peft_method") == "lora"
+        else prompt_tuning_config,
+    )
