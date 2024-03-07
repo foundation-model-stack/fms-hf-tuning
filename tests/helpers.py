09fb92005a262929c01dd4a4eed4f66aebcb0609
Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>
2024-03-07 09:49:23 -0800

    Add basic unit tests
    Signed-off-by: Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>

---
+++ tests/helpers.py
@@ -0,0 +1,19 @@
+# Third Party
+import transformers
+
+# Local
+from tuning.config import configs, peft_config
+
+def causal_lm_train_kwargs(train_kwargs):
+    """Parse the kwargs for a valid train call to a Causal LM."""
+    parser = transformers.HfArgumentParser(
+    dataclass_types=(
+        configs.ModelArguments,
+        configs.DataArguments,
+        configs.TrainingArguments,
+        peft_config.LoraConfig,
+        peft_config.PromptTuningConfig,
+        )
+    )
+    model_args, data_args, training_args, lora_config, prompt_tuning_config = parser.parse_dict(train_kwargs, allow_extra_keys=True)
+    return model_args, data_args, training_args, lora_config if train_kwargs.get("peft_method")=="lora" else prompt_tuning_config
