d5723e9c4e8d4be121c9e4cb64ec52398f482fb6
Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>
2024-03-19 22:09:51 -0700

    Modify copyright notice
    Signed-off-by: Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>

--- tuning/aim_loader.py
+++ tuning/aim_loader.py
@@ -1,4 +1,4 @@
-# Copyright The IBM Tuning Team
+# Copyright The FMS HF Tuning Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
