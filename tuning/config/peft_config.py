df6737d61778b061e7476d8deebf5692e17540cf
Sukriti Sharma <Ssukriti@users.noreply.github.com>
2024-04-08 08:07:45 -0600

    Docs improvements (#111)
    * improve docs for tuning techniques
    
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
    
    * formatting fixes
    
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
    
    * format files
    
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
    
    * pylint fixes
    
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
    
    * pylint fixes
    
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
    
    ---------
    
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>

--- tuning/config/peft_config.py
+++ tuning/config/peft_config.py
@@ -19,6 +19,30 @@ from typing import List
 
 @dataclass
 class LoraConfig:
+    """
+    This is the configuration class to store the configuration of a [`LoraModel`].
+
+    Args:
+        r (`int`):
+            Lora attention dimension (the "rank").
+        target_modules (List[str]]):
+            The names of the modules to apply the adapter to. \
+            If this is specified, only the modules with the specified \
+            names will be replaced. Please specify modules as per model architecture. \
+            If the value is ["all-linear"], \
+            then LORA selects all linear and Conv1D modules as per model architecture, \
+            except for the output layer.
+        lora_alpha (`int`):
+            The alpha parameter for Lora scaling.
+        lora_dropout (`float`):
+            The dropout probability for Lora layers.
+        bias (`str`):
+            Bias type for LoRA. Can be 'none', 'all' or 'lora_only'. \
+            If 'all' or 'lora_only', the corresponding biases will be updated during training. \
+            Be aware that this means that, even when disabling the adapters, the model \
+            will not produce the same output as the base model would have without adaptation.
+    """
+
     r: int = 8
     lora_alpha: int = 32
     target_modules: List[str] = field(
@@ -37,6 +61,21 @@ class LoraConfig:
 
 @dataclass
 class PromptTuningConfig:
+    """
+    This is the configuration class for Prompt Tuning.
+
+    Args:
+        prompt_tuning_init : str: The initialization of the prompt embedding. \
+            Allowed values "TEXT" or "RANDOM".
+        prompt_tuning_init_text (`str`, *optional*):
+            The text to initialize the prompt embedding. \
+            Only used if `prompt_tuning_init` is `TEXT`.
+        tokenizer_name_or_path (`str`, *optional*):
+            The name or path of the tokenizer. \
+            Only used if `prompt_tuning_init` is `TEXT`.
+        num_virtual_tokens (`int`): The number of virtual tokens to use.
+    """
+
     prompt_tuning_init: str = "TEXT"
     num_virtual_tokens: int = 8
     prompt_tuning_init_text: str = "Classify if the tweet is a complaint or not:"
