264aa1984e6959b63b5ccb495f6d2496b67f939b
Anh Uong <anh.uong@ibm.com>
2024-04-08 09:26:00 -0600

    feat: add env var SET_NUM_PROCESSES_TO_NUM_GPUS (#110)
    * add env var SET_NUM_PROCESSES_TO_NUM_GPUS
    
    Signed-off-by: Anh-Uong <anh.uong@ibm.com>
    
    * fix num processes type
    
    Signed-off-by: Anh-Uong <anh.uong@ibm.com>
    
    * move num_processes logic into build.utils
    
    Signed-off-by: Anh-Uong <anh.uong@ibm.com>
    
    * add unit tests for num_processes
    
    Signed-off-by: Anh-Uong <anh.uong@ibm.com>
    
    * refactor duplicate test case
    
    Signed-off-by: Anh-Uong <anh.uong@ibm.com>
    
    ---------
    
    Signed-off-by: Anh-Uong <anh.uong@ibm.com>
    Co-authored-by: Sukriti Sharma <Ssukriti@users.noreply.github.com>

--- tests/build/test_utils.py
+++ tests/build/test_utils.py
@@ -82,34 +82,73 @@ def test_process_launch_training_args_peft_method(job_config):
 
 
 def test_process_accelerate_launch_args(job_config):
-    job_config_copy = copy.deepcopy(job_config)
-    args = process_accelerate_launch_args(job_config_copy)
+    args = process_accelerate_launch_args(job_config)
+    # json config values used
+    assert args.use_fsdp is True
+    assert args.fsdp_backward_prefetch_policy == "TRANSFORMER_BASED_WRAP"
+    assert args.env == ["env1", "env2"]
+    assert args.training_script == "/app/launch_training.py"
     assert args.config_file == "fixtures/accelerate_fsdp_defaults.yaml"
-    assert args.use_fsdp == True
-    assert args.tpu_use_cluster == False
+
+    # default values
+    assert args.tpu_use_cluster is False
+    assert args.mixed_precision is None
+
+
+@patch("torch.cuda.device_count", return_value=1)
+def test_accelerate_launch_args_user_set_num_processes_ignored(job_config):
+    job_config_copy = copy.deepcopy(job_config)
+    job_config_copy["accelerate_launch_args"]["num_processes"] = "3"
+    args = process_accelerate_launch_args(job_config_copy)
+    # determine number of processes by number of GPUs available
+    assert args.num_processes == 1
+
+    # if single-gpu, CUDA_VISIBLE_DEVICES set
+    assert os.getenv("CUDA_VISIBLE_DEVICES") == "0"
+
+
+@patch.dict(os.environ, {"SET_NUM_PROCESSES_TO_NUM_GPUS": "False"})
+def test_accelerate_launch_args_user_set_num_processes(job_config):
+    job_config_copy = copy.deepcopy(job_config)
+    job_config_copy["accelerate_launch_args"]["num_processes"] = "3"
+
+    args = process_accelerate_launch_args(job_config_copy)
+    # json config values used
+    assert args.num_processes == 3
+    assert args.config_file == "fixtures/accelerate_fsdp_defaults.yaml"
+
+
+def test_accelerate_launch_args_default_fsdp_config_multigpu(job_config):
+    with patch("torch.cuda.device_count", return_value=2):
+        with patch("os.path.exists", return_value=True):
+            job_config_copy = copy.deepcopy(job_config)
+            job_config_copy["accelerate_launch_args"].pop("config_file")
+
+            assert "config_file" not in job_config_copy["accelerate_launch_args"]
+
+            args = process_accelerate_launch_args(job_config_copy)
+
+            # use default config file
+            assert args.config_file == "/app/accelerate_fsdp_defaults.yaml"
+            # determine number of processes by number of GPUs available
+            assert args.num_processes == 2
 
 
 @patch("os.path.exists")
-def test_process_accelerate_launch_custom_fsdp(patch_path_exists):
+def test_process_accelerate_launch_custom_config_file(patch_path_exists):
     patch_path_exists.return_value = True
 
-    dummy_fsdp_path = "dummy_fsdp_config.yaml"
+    dummy_config_path = "dummy_fsdp_config.yaml"
 
     # When user passes custom fsdp config file, use custom config and accelerate
     # launch will use `num_processes` from config
-    temp_job_config = {"accelerate_launch_args": {"config_file": dummy_fsdp_path}}
+    temp_job_config = {"accelerate_launch_args": {"config_file": dummy_config_path}}
     args = process_accelerate_launch_args(temp_job_config)
-    assert args.config_file == dummy_fsdp_path
-    assert args.num_processes == None
+    assert args.config_file == dummy_config_path
+    assert args.num_processes is None
 
     # When user passes custom fsdp config file and also `num_processes` as a param, use custom config and
     # overwrite num_processes from config with param
-    temp_job_config = {
-        "accelerate_launch_args": {
-            "config_file": dummy_fsdp_path,
-            "num_processes": 3,
-        }
-    }
+    temp_job_config = {"accelerate_launch_args": {"config_file": dummy_config_path}}
     args = process_accelerate_launch_args(temp_job_config)
-    assert args.config_file == dummy_fsdp_path
-    assert args.num_processes == 3
+    assert args.config_file == dummy_config_path
