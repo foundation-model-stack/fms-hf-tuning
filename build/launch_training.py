38c4f22219f296079470f60641872a6bdefe887f
Kelly A <kellyaa@users.noreply.github.com>
2024-05-14 17:26:20 -0400

    Add exception catching / writing to termination log (#149)
    * Rebase exception handling in launch_training
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * add exception catching to accelerate launch script
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * linter fixes
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * 🧹 linting fixes for launch scripts
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * 🧹 fmt fixes for launch scripts
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * Make exit codes configurable
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * Add tests for accelerate_launch
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * Add launch script var, fix linting
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * Fail job when logs cannot be copied
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * fix rebase mistake
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * fix rebase mistake
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * Exception catching improvements
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    * Update model loading exception message
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>

--- build/launch_training.py
+++ build/launch_training.py
@@ -21,6 +21,12 @@ for the encoded config string to parse.
 import os
 import tempfile
 import shutil
+import sys
+import traceback
+
+# Third Party
+from huggingface_hub.utils._validators import HFValidationError
+from torch.cuda import OutOfMemoryError
 
 # First Party
 import logging
@@ -29,7 +35,13 @@ import logging
 from tuning import sft_trainer
 from tuning.utils.merge_model_utils import create_merged_model
 from tuning.config.tracker_configs import TrackerConfigFactory
-from build.utils import process_launch_training_args, get_job_config
+from build.utils import (
+    process_launch_training_args,
+    get_job_config,
+    write_termination_log,
+    USER_ERROR_EXIT_CODE,
+    INTERNAL_ERROR_EXIT_CODE,
+)
 
 
 def get_highest_checkpoint(dir_path):
@@ -53,78 +65,132 @@ def main():
 
     logging.info("Initializing launch training script")
 
-    job_config = get_job_config()
+    try:
+        job_config = get_job_config()
+        logging.debug("Input params parsed: %s", job_config)
 
-    logging.debug("Input params parsed: %s", job_config)
-
-    (
-        model_args,
-        data_args,
-        training_args,
-        tune_config,
-        merge_model,
-        file_logger_config,
-        aim_config,
-    ) = process_launch_training_args(job_config)
+        (
+            model_args,
+            data_args,
+            training_args,
+            tune_config,
+            merge_model,
+            file_logger_config,
+            aim_config,
+        ) = process_launch_training_args(job_config)
+    except Exception as e:  # pylint: disable=broad-except
+        logging.error(traceback.format_exc())
+        write_termination_log(
+            f"Exception raised during training. This may be a problem with your input: {e}"
+        )
+        sys.exit(USER_ERROR_EXIT_CODE)
 
     original_output_dir = training_args.output_dir
     with tempfile.TemporaryDirectory() as tempdir:
         training_args.output_dir = tempdir
-        tracker_config_args = TrackerConfigFactory(
-            file_logger_config=file_logger_config, aim_config=aim_config
-        )
-        sft_trainer.train(
-            model_args=model_args,
-            data_args=data_args,
-            train_args=training_args,
-            peft_config=tune_config,
-            tracker_configs=tracker_config_args,
-        )
+        try:
+            tracker_config_args = TrackerConfigFactory(
+                file_logger_config=file_logger_config, aim_config=aim_config
+            )
+            sft_trainer.train(
+                model_args=model_args,
+                data_args=data_args,
+                train_args=training_args,
+                peft_config=tune_config,
+                tracker_configs=tracker_config_args,
+            )
+        except (MemoryError, OutOfMemoryError) as e:
+            logging.error(traceback.format_exc())
+            write_termination_log(f"OOM error during training. {e}")
+            sys.exit(INTERNAL_ERROR_EXIT_CODE)
+        except FileNotFoundError as e:
+            logging.error(traceback.format_exc())
+            write_termination_log("Unable to load file: {}".format(e))
+            sys.exit(USER_ERROR_EXIT_CODE)
+        except HFValidationError as e:
+            logging.error(traceback.format_exc())
+            write_termination_log(
+                f"There may be a problem with loading the model. Exception: {e}"
+            )
+            sys.exit(USER_ERROR_EXIT_CODE)
+        except (TypeError, ValueError, EnvironmentError) as e:
+            logging.error(traceback.format_exc())
+            write_termination_log(
+                f"Exception raised during training. This may be a problem with your input: {e}"
+            )
+            sys.exit(USER_ERROR_EXIT_CODE)
+        except Exception as e:  # pylint: disable=broad-except
+            logging.error(traceback.format_exc())
+            write_termination_log(f"Unhandled exception during training: {e}")
+            sys.exit(INTERNAL_ERROR_EXIT_CODE)
 
         if merge_model:
-            export_path = os.getenv(
-                "LORA_MERGE_MODELS_EXPORT_PATH", original_output_dir
-            )
+            try:
+                export_path = os.getenv(
+                    "LORA_MERGE_MODELS_EXPORT_PATH", original_output_dir
+                )
 
-            # get the highest checkpoint dir (last checkpoint)
-            lora_checkpoint_dir = get_highest_checkpoint(training_args.output_dir)
-            full_checkpoint_dir = os.path.join(
-                training_args.output_dir, lora_checkpoint_dir
-            )
+                # get the highest checkpoint dir (last checkpoint)
+                lora_checkpoint_dir = get_highest_checkpoint(training_args.output_dir)
+                full_checkpoint_dir = os.path.join(
+                    training_args.output_dir, lora_checkpoint_dir
+                )
 
-            logging.info(
-                "Merging lora tuned checkpoint %s with base model into output path: %s",
-                lora_checkpoint_dir,
-                export_path,
-            )
+                logging.info(
+                    "Merging lora tuned checkpoint %s with base model into output path: %s",
+                    lora_checkpoint_dir,
+                    export_path,
+                )
 
-            create_merged_model(
-                checkpoint_models=full_checkpoint_dir,
-                export_path=export_path,
-                base_model=model_args.model_name_or_path,
-                save_tokenizer=True,
-            )
+                create_merged_model(
+                    checkpoint_models=full_checkpoint_dir,
+                    export_path=export_path,
+                    base_model=model_args.model_name_or_path,
+                    save_tokenizer=True,
+                )
+            except Exception as e:  # pylint: disable=broad-except
+                logging.error(traceback.format_exc())
+                write_termination_log(
+                    f"Exception encountered merging base model with checkpoint. {e}"
+                )
+                sys.exit(INTERNAL_ERROR_EXIT_CODE)
         else:
-            # copy last checkpoint into mounted output dir
-            pt_checkpoint_dir = get_highest_checkpoint(training_args.output_dir)
-            logging.info(
-                "Copying last checkpoint %s into output dir %s",
-                pt_checkpoint_dir,
-                original_output_dir,
-            )
-            shutil.copytree(
-                os.path.join(training_args.output_dir, pt_checkpoint_dir),
-                original_output_dir,
-                dirs_exist_ok=True,
-            )
+            try:
+                # copy last checkpoint into mounted output dir
+                pt_checkpoint_dir = get_highest_checkpoint(training_args.output_dir)
+                logging.info(
+                    "Copying last checkpoint %s into output dir %s",
+                    pt_checkpoint_dir,
+                    original_output_dir,
+                )
+                shutil.copytree(
+                    os.path.join(training_args.output_dir, pt_checkpoint_dir),
+                    original_output_dir,
+                    dirs_exist_ok=True,
+                )
+            except Exception as e:  # pylint: disable=broad-except
+                logging.error(traceback.format_exc())
+                write_termination_log(
+                    f"Exception encountered writing output model to storage: {e}"
+                )
+                sys.exit(INTERNAL_ERROR_EXIT_CODE)
 
         # copy over any loss logs
-        train_logs_filepath = os.path.join(
-            training_args.output_dir,
-            tracker_config_args.file_logger_config.training_logs_filename,
-        )
-        if os.path.exists(train_logs_filepath):
-            shutil.copy(train_logs_filepath, original_output_dir)
+        try:
+            train_logs_filepath = os.path.join(
+                training_args.output_dir,
+                tracker_config_args.file_logger_config.training_logs_filename,
+            )
+            if os.path.exists(train_logs_filepath):
+                shutil.copy(train_logs_filepath, original_output_dir)
+        except Exception as e:  # pylint: disable=broad-except
+            logging.error(traceback.format_exc())
+            write_termination_log(
+                f"Exception encountered in capturing training logs: {e}"
+            )
+            sys.exit(INTERNAL_ERROR_EXIT_CODE)
+
+    return 0
 
 
 if __name__ == "__main__":
