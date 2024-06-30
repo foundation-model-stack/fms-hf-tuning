9496f2f5ebb7cd8c88fbcfd2efd31bbb48d9bef3
Yu Chin Fabian Lim <flim@sg.ibm.com>
2024-06-30 15:46:07 +0000

    revert some changes in #219
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- tuning/config/acceleration_configs/acceleration_framework_config.py
+++ tuning/config/acceleration_configs/acceleration_framework_config.py
@@ -178,10 +178,10 @@ class AccelerationFrameworkConfig:
         else:
             if not self.is_empty():
                 raise ValueError(
-                    "No acceleration framework package found. To use, first ensure that "
-                    "'pip install git+https://github.com/foundation-model-stack/fms-acceleration.git#subdirectory=plugins/framework' "  # pylint: disable=line-too-long
-                    "is done first to obtain the acceleration framework dependency. Additional "
-                    "acceleration plugins make be required depending on the requested "
+                    "No acceleration framework package found. To use, first "
+                    "ensure that 'pip install fms-hf-tuning[fms-accel]' is done first to "
+                    "obtain the acceleration framework dependency. Additional "
+                    "acceleration plugins make be required depending on the requsted "
                     "acceleration. See README.md for instructions."
                 )
 
