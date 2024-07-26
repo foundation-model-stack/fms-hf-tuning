7dfd4e71a0ded17ab65654925e18bf9a1d76b0fc
Joe Olson <118190512+olson-ibm@users.noreply.github.com>
2024-07-26 11:24:52 -0500

    Replace shutil.copytree() to fix permission error (#251)
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * added unit tests, fixed other issues raised by review.
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes 1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #1089
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    ---------
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    Co-authored-by: Anh Uong <anh.uong@ibm.com>

--- tests/build/test_utils.py
+++ tests/build/test_utils.py
@@ -17,12 +17,15 @@ import copy
 import json
 import os
 from unittest.mock import patch
+import tempfile
 
 # Third Party
 import pytest
+import filecmp
 
 # Local
 from build.utils import process_accelerate_launch_args
+from build.utils import copy_checkpoint
 
 HAPPY_PATH_DUMMY_CONFIG_PATH = os.path.join(
     os.path.dirname(__file__), "dummy_job_config.json"
@@ -108,3 +111,132 @@ def test_process_accelerate_launch_custom_config_file(patch_path_exists):
     temp_job_config = {"accelerate_launch_args": {"config_file": dummy_config_path}}
     args = process_accelerate_launch_args(temp_job_config)
     assert args.config_file == dummy_config_path
+
+
+class CopyCheckpointTestConfig:
+    def __init__(self, temp_root):
+
+        # Create the following file tree for testing:
+        # test_root
+        #   test_copytree_source
+        #      tf1.txt
+        #      tf2.txt
+        #      tf3.txt
+        #      subdir1
+        #         tf4.txt
+        #         tf5.txt
+        #         tf6.txt
+
+        self.test_root = temp_root
+        self.source_dir = os.path.join(self.test_root, "test_copytree_source")
+        self.source_sub_dir = os.path.join(self.source_dir, "subdir1")
+
+        os.mkdir(self.source_dir)
+        for file_number in range(2):
+            with open(
+                os.path.join(self.source_dir, f"tf{file_number+1}.txt"),
+                "a",
+                encoding="utf-8",
+            ) as f:
+                f.close()
+
+        os.mkdir(self.source_sub_dir)
+        for file_number in range(2):
+            with open(
+                os.path.join(self.source_sub_dir, f"tf{file_number+4}.txt"),
+                "a",
+                encoding="utf-8",
+            ) as f:
+                f.close()
+
+    def are_dir_trees_equal(self, dir1, dir2):
+
+        dirs_cmp = filecmp.dircmp(dir1, dir2)
+        if (
+            len(dirs_cmp.left_only) > 0
+            or len(dirs_cmp.right_only) > 0
+            or len(dirs_cmp.funny_files) > 0
+        ):
+            return False
+        (_, mismatch, errors) = filecmp.cmpfiles(
+            dir1, dir2, dirs_cmp.common_files, shallow=False
+        )
+        if len(mismatch) > 0 or len(errors) > 0:
+            return False
+        for common_dir in dirs_cmp.common_dirs:
+            new_dir1 = os.path.join(dir1, common_dir)
+            new_dir2 = os.path.join(dir2, common_dir)
+            if not self.are_dir_trees_equal(new_dir1, new_dir2):
+                return False
+        return True
+
+
+def test_copy_checkpoint_dest_dir_does_not_exist():
+
+    # Init source directory
+    with tempfile.TemporaryDirectory() as test_root:
+        config = CopyCheckpointTestConfig(test_root)
+
+        target_dir_does_not_exist = os.path.join(
+            config.test_root, "test_copytree_target"
+        )
+
+        # Execute the copy
+        copy_checkpoint(config.source_dir, target_dir_does_not_exist)
+        assert config.are_dir_trees_equal(config.source_dir, target_dir_does_not_exist)
+
+
+def test_copy_checkpoint_dest_dir_does_exist():
+
+    # Init source directory
+    with tempfile.TemporaryDirectory() as test_root:
+        config = CopyCheckpointTestConfig(test_root)
+
+        # Init target directory
+        target_dir_does_exist = os.path.join(config.test_root, "test_copytree_target2")
+        os.mkdir(target_dir_does_exist)
+        # Add a file to the target. This file will be overwritten during the copy.
+        with open(
+            os.path.join(target_dir_does_exist, "tf1.txt"),
+            "a",
+            encoding="utf-8",
+        ) as f:
+            f.close()
+        # Add a file to the target. This file does not exist in source.
+        with open(
+            os.path.join(target_dir_does_exist, "tf9.txt"),
+            "a",
+            encoding="utf-8",
+        ) as f:
+            f.close()
+        # Execute the copy
+        copy_checkpoint(config.source_dir, target_dir_does_exist)
+        assert os.path.exists(os.path.join(target_dir_does_exist, "tf9.txt"))
+        # Remove it so we can validate the dir trees are equal.
+        os.remove(os.path.join(target_dir_does_exist, "tf9.txt"))
+        assert config.are_dir_trees_equal(config.source_dir, target_dir_does_exist)
+
+
+def test_copy_checkpoint_dest_dir_not_writeable():
+
+    # Init source directory
+    with tempfile.TemporaryDirectory() as test_root:
+        config = CopyCheckpointTestConfig(test_root)
+
+        # Init target directory
+        target_dir_not_writeable = os.path.join(
+            config.test_root, "test_copytree_notwriteable"
+        )
+
+        os.makedirs(target_dir_not_writeable, mode=0o446)
+
+        # Execute the copy. Should FAIL
+        with pytest.raises(PermissionError) as e:
+            copy_checkpoint(config.source_dir, target_dir_not_writeable)
+        assert "Permission denied:" in str(e.value)
+
+
+def test_copy_checkpoint_source_dir_does_not_exist():
+    with pytest.raises(FileNotFoundError) as e:
+        copy_checkpoint("/doesnotexist", "/tmp")
+    assert "No such file or directory" in str(e.value)
