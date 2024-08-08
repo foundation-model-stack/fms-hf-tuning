89124ac9358b0b290c91d7e1a829664d76776853
Abhishek <maurya.abhishek@ibm.com>
2024-08-08 17:03:44 -0400

    Revert last commit
    Signed-off-by: Abhishek <maurya.abhishek@ibm.com>

--- tuning/trackers/filelogging_tracker.py
+++ tuning/trackers/filelogging_tracker.py
@@ -56,9 +56,7 @@ class FileLoggingCallback(TrainerCallback):
             log_obj = {
                 "name": log_name,
                 "data": {
-                    "epoch": round((logs["epoch"] - 1), 2)
-                    if logs["epoch"] != 1.0
-                    else 0,
+                    "epoch": round(logs["epoch"], 2),
                     "step": state.global_step,
                     "value": logs[loss_key],
                     "timestamp": datetime.isoformat(datetime.now()),
