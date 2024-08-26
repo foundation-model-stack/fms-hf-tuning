1e64f22881ad7fea7f8169760695374c07261719
Padmanabha V Seshadri <seshapad@in.ibm.com>
2024-08-26 00:36:03 -0400

    fix: Added default logging level to DEBUG
    Signed-off-by: Padmanabha V Seshadri <seshapad@in.ibm.com>

--- tuning/trainercontroller/patience.py
+++ tuning/trainercontroller/patience.py
@@ -34,6 +34,7 @@ MODE_NO_RESET_ON_FAILURE = "no_reset_on_failure"
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 class PatienceControl:
     """Implements the patience control for every rule"""
 
