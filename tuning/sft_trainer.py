57f4ff0da7a6d33a9255ee762b7a5b565c4ff907
Will Johnson <mwjohnson728@gmail.com>
2024-08-28 11:01:41 -0400

    fix: set legacy behavior to false, enable new behavior
    Signed-off-by: Will Johnson <mwjohnson728@gmail.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -200,6 +200,7 @@ def train(
         ),
         cache_dir=train_args.cache_dir,
         use_fast=True,
+        legacy=False,
     )
 
     # Calculate and save additional metrics to track later.
