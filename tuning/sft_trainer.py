9c398d34a1a588589fdced9e6f483ca93578bfc3
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2024-10-02 12:45:59 +0530

    fix: crash when output directory doesn't exist
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -635,6 +635,9 @@ def main():
     combined_tracker_configs.file_logger_config = file_logger_config
     combined_tracker_configs.aim_config = aim_config
 
+    if model_args.output_dir:
+        os.makedirs(model_args.output_dir, exist_ok=True)
+        logger.info("using the output directory at %s", model_args.output_dir)
     try:
         trainer, additional_train_info = train(
             model_args=model_args,
