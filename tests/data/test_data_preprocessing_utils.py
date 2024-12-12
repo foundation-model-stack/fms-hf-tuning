5905e235ff7175ef19b00bc83496a87ef702d974
Abhishek <maurya.abhishek@ibm.com>
2024-12-12 18:58:24 -0500

    Merge test_process_dataconfig_multiple_files_varied_data_formats
    Signed-off-by: Abhishek <maurya.abhishek@ibm.com>

--- tests/data/test_data_preprocessing_utils.py
+++ tests/data/test_data_preprocessing_utils.py
@@ -683,6 +683,10 @@ def test_process_dataconfig_multiple_datasets_datafiles_sampling(
             [TWITTER_COMPLAINTS_DATA_JSON, TWITTER_COMPLAINTS_DATA_PARQUET],
         ),
         (
+            DATA_CONFIG_APPLY_CUSTOM_TEMPLATE_YAML,
+            [TWITTER_COMPLAINTS_DATA_JSON, TWITTER_COMPLAINTS_TOKENIZED_JSON],
+        ),
+        (
             DATA_CONFIG_PRETOKENIZE_JSON_DATA_YAML,
             [
                 TWITTER_COMPLAINTS_TOKENIZED_JSONL,
@@ -691,11 +695,22 @@ def test_process_dataconfig_multiple_datasets_datafiles_sampling(
             ],
         ),
         (
+            DATA_CONFIG_PRETOKENIZE_JSON_DATA_YAML,
+            [
+                TWITTER_COMPLAINTS_TOKENIZED_JSON,
+                TWITTER_COMPLAINTS_DATA_INPUT_OUTPUT_JSON,
+            ],
+        ),
+        (
             DATA_CONFIG_TOKENIZE_AND_APPLY_INPUT_MASKING_YAML,
             [
                 TWITTER_COMPLAINTS_DATA_INPUT_OUTPUT_PARQUET,
                 TWITTER_COMPLAINTS_DATA_INPUT_OUTPUT_JSONL,
             ],
+        ),
+        (
+            DATA_CONFIG_TOKENIZE_AND_APPLY_INPUT_MASKING_YAML,
+            [TWITTER_COMPLAINTS_DATA_INPUT_OUTPUT_JSON, TWITTER_COMPLAINTS_DATA_JSON],
         ),
     ],
 )
@@ -732,64 +747,9 @@ def test_process_dataconfig_multiple_files_varied_data_formats(
         data_args = configs.DataArguments(data_config_path=temp_yaml_file_path)
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-    with pytest.raises(AssertionError):
-        (_, _, _) = _process_dataconfig_file(data_args, tokenizer)
-
-
-@pytest.mark.parametrize(
-    "data_config_path, data_path_list",
-    [
-        (
-            DATA_CONFIG_APPLY_CUSTOM_TEMPLATE_YAML,
-            [TWITTER_COMPLAINTS_DATA_JSON, TWITTER_COMPLAINTS_TOKENIZED_JSON],
-        ),
-        (
-            DATA_CONFIG_PRETOKENIZE_JSON_DATA_YAML,
-            [
-                TWITTER_COMPLAINTS_TOKENIZED_JSON,
-                TWITTER_COMPLAINTS_DATA_INPUT_OUTPUT_JSON,
-            ],
-        ),
-        (
-            DATA_CONFIG_TOKENIZE_AND_APPLY_INPUT_MASKING_YAML,
-            [TWITTER_COMPLAINTS_DATA_INPUT_OUTPUT_JSON, TWITTER_COMPLAINTS_DATA_JSON],
-        ),
-    ],
-)
-def test_process_dataconfig_multiple_files_varied_types(
-    data_config_path, data_path_list
-):
-    """Ensure that datasets with multiple files with different formats raise assertion error when passed in config file."""
-    with open(data_config_path, "r") as f:
-        yaml_content = yaml.safe_load(f)
-    yaml_content["datasets"][0]["data_paths"] = data_path_list
-    datasets_name = yaml_content["datasets"][0]["name"]
-
-    # Modify input_field_name and output_field_name according to dataset
-    if datasets_name == "text_dataset_input_output_masking":
-        yaml_content["datasets"][0]["data_handlers"][0]["arguments"]["fn_kwargs"] = {
-            "input_field_name": "input",
-            "output_field_name": "output",
-        }
-
-    # Modify dataset_text_field and template according to dataset
-    formatted_dataset_field = "formatted_data_field"
-    if datasets_name == "apply_custom_data_template":
-        template = "### Input: {{Tweet text}} \n\n ### Response: {{text_label}}"
-        yaml_content["datasets"][0]["data_handlers"][0]["arguments"]["fn_kwargs"] = {
-            "dataset_text_field": formatted_dataset_field,
-            "template": template,
-        }
-
-    with tempfile.NamedTemporaryFile(
-        "w", delete=False, suffix=".yaml"
-    ) as temp_yaml_file:
-        yaml.dump(yaml_content, temp_yaml_file)
-        temp_yaml_file_path = temp_yaml_file.name
-        data_args = configs.DataArguments(data_config_path=temp_yaml_file_path)
-
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-    with pytest.raises(datasets.exceptions.DatasetGenerationCastError):
+    with pytest.raises(
+        (AssertionError, datasets.exceptions.DatasetGenerationCastError)
+    ):
         (_, _, _) = _process_dataconfig_file(data_args, tokenizer)
 
 
