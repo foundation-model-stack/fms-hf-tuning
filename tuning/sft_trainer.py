9e567e2576c24f380207521fbd2b04befc2ba3cb
Luka Dojcinovic <56648891+Luka-D@users.noreply.github.com>
2025-01-07 11:55:25 -0500

    fix: Changed to_dict() to __dict__
    Because of this change, also had to change how the field names are obtained from SFTConfig.
    
    Signed-off-by: Luka Dojcinovic <56648891+Luka-D@users.noreply.github.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -330,15 +330,12 @@ def train(
     # from our object directly. In the future, we should consider renaming this class and / or
     # not adding things that are not directly used by the trainer instance to it.
 
-    transformer_train_arg_fields = [x.name for x in dataclasses.fields(SFTConfig)]
+    transformer_train_arg_fields = SFTConfig.__dict__["__match_args__"]
     transformer_kwargs = {
         k: v
-        for k, v in train_args.to_dict().items()
+        for k, v in train_args.__dict__.items()
         if k in transformer_train_arg_fields
     }
-
-    # Remove deprecated push_to_hub_token
-    transformer_kwargs.pop("push_to_hub_token", None)
 
     additional_args = {
         "dataset_text_field": data_args.dataset_text_field,
