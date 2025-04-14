fe3b5af954d7798e4970c82f64b976c2c1d7e083
Abhishek <maurya.abhishek@ibm.com>
2025-04-14 13:17:37 -0400

    Remove enable_reduce_loss_sum and _is_peft_model check
    Signed-off-by: Abhishek <maurya.abhishek@ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -27,7 +27,6 @@ from huggingface_hub.utils._validators import HFValidationError
 from peft.utils.other import fsdp_auto_wrap_policy
 from torch.cuda import OutOfMemoryError
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainerCallback
-from transformers.trainer import _is_peft_model
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import is_accelerate_available
 from trl import SFTConfig, SFTTrainer
@@ -367,13 +366,6 @@ def train(
         callbacks=trainer_callbacks,
         peft_config=peft_config,
     )
-
-    # Note this check has to be done post trainer initialization
-    if train_args.enable_reduce_loss_sum and _is_peft_model(trainer.model):
-        raise ValueError(
-            "❌ Loaded model is PEFT model but both PEFT and sum loss is not supported yet."
-            + "Set --enable_reduce_loss_sum to false and retry"
-        )
 
     # We track additional metrics and experiment metadata after trainer object creation
     # this ensure that the process is not repeated multiple times for FSDP runs.
