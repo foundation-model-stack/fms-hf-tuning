4289e4cbcc366317a60fd5a68e6ee6af403b4c3c
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2025-04-19 19:20:17 +0530

    feat: allow specifying the chat template as base64 to avoid weird escaping and templating issues
    test: create tests for chat_template_base64 field
    
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- tuning/data/data_config.py
+++ tuning/data/data_config.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 # Standard
+from base64 import b64decode
 from dataclasses import dataclass
 from typing import Dict, List, Optional
 import logging
@@ -153,6 +154,24 @@ def _validate_dataprocessor_config(dataprocessor_config) -> DataPreProcessorConf
         chat_template = kwargs["chat_template"]
         assert isinstance(chat_template, str), "chat_template should be a string"
         c.chat_template = chat_template
+    elif "chat_template_base64" in kwargs:
+        chat_template_base64 = kwargs["chat_template_base64"]
+        assert isinstance(
+            chat_template_base64, str
+        ), "chat_template_base64 should be a string"
+        logger.warning(
+            "You are using the 'chat_template_base64' field. "
+            + "Please use the 'chat_template' field instead for better readability."
+        )
+        try:
+            chat_template_bytes = b64decode(chat_template_base64)
+            chat_template = chat_template_bytes.decode("utf-8")
+            c.chat_template = chat_template
+        except Exception as e:
+            raise ValueError(
+                "You passed the 'chat_template_base64' field which failed during decoding."
+                + "Please check it or use a decoded chat template with the 'chat_template' field."
+            ) from e
     return c
 
 
