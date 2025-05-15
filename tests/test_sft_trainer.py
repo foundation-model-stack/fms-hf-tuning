c9b43b638070203aba37eeeb9a97212782f8d29c
Greenewald <greenewk@umich.edu>
2025-05-14 21:28:38 -0400

    Update test_sft_trainer.py
    Signed-off-by: Greenewald <greenewk@umich.edu>

--- tests/test_sft_trainer.py
+++ tests/test_sft_trainer.py
@@ -746,7 +746,7 @@ def test_run_causallm_alora_and_inference(request, target_modules, expected):
 
         # Load the model
         loaded_model = TunedCausalLM.load(checkpoint_path, MODEL_NAME, use_alora=True)
-        invocation_string = loaded_model.peft_config[model_UQ.active_adapter].invocation_string
+        invocation_string = loaded_model.peft_model.peft_config[loaded_model.peft_model.active_adapter].invocation_string
         alora_offsets = loaded_model.tokenizer(invocation_string, return_tensors="pt").input_ids.shape[1] - 1
         # Run inference on the text
         output_inference = loaded_model.run(
