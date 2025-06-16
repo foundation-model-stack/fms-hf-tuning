8fc0cccb68ff3cd3bb470aa9e54e8a853c9e0cb2
Padmanabha V Seshadri <seshapad@in.ibm.com>
2025-06-16 04:50:51 +0000

    fix: Test case return value failure
    Signed-off-by: Padmanabha V Seshadri <seshapad@in.ibm.com>

--- tests/test_sft_trainer.py
+++ tests/test_sft_trainer.py
@@ -847,8 +847,10 @@ def test_run_causallm_ft_save_with_save_model_dir_save_strategy_no():
         save_model_args.save_strategy = "no"
         save_model_args.output_dir = tempdir
 
-        trainer, _ = sft_trainer.train(MODEL_ARGS, DATA_ARGS, save_model_args, None)
-        logs_path = os.path.join(tempdir, TrackerConfigs.training_logs_filename)
+        trainer, _, _ = sft_trainer.train(MODEL_ARGS, DATA_ARGS, save_model_args, None)
+        logs_path = os.path.join(
+            tempdir, FileLoggingTrackerConfig.training_logs_filename
+        )
         _validate_logfile(logs_path)
         # validate that no checkpoints created
         assert not any(x.startswith("checkpoint-") for x in os.listdir(tempdir))
