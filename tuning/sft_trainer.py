a2ecfa577d46e73c9ab5ad0cfe99b51d621576fc
Padmanabha V Seshadri <seshapad@in.ibm.com>
2025-07-09 22:43:06 +0530

    fix: Lint issues
    Signed-off-by: Padmanabha V Seshadri <seshapad@in.ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -508,8 +508,7 @@ def save(path: str, trainer: SFTTrainer, tc_callback, log_level="WARNING", args=
     logger.info("Saving tuned model to path: %s", path)
     trainer.save_model(path)
     if tc_callback and args:
-        kwargs["path"] = path
-        tc_callback.on_save(args, trainer.state, trainer.control, is_final=True)
+        tc_callback.on_save(args, trainer.state, trainer.control, path=path, is_final=True)
 
 
 def get_parser():
