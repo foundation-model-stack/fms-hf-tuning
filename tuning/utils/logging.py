f0b55bf39a8cb1192fe16781a68dd5d14499bd30
Dushyant Behl <dushyantbehl@users.noreply.github.com>
2025-07-11 20:19:39 +0530

    fix: change logging level to info and print flat arguments (#582)
    Signed-off-by: Dushyant Behl <dushyantbehl@in.ibm.com>

--- tuning/utils/logging.py
+++ tuning/utils/logging.py
@@ -13,24 +13,33 @@
 # limitations under the License.
 
 # Standard
+from typing import Dict
 import logging
 import os
 
+# Third Party
+from accelerate.state import PartialState
+import datasets
+import transformers
 
-def set_log_level(train_args, logger_name=None):
+DEFAULT_LOG_LEVEL_MAIN = "INFO"
+DEFAULT_LOG_LEVEL_WORKERS = "WARNING"
+
+
+def set_log_level(logger_name="fms-hf-tuning", level=None):
     """Set log level of python native logger and TF logger via argument from CLI or env variable.
 
     Args:
-        train_args
-            Training arguments for training model.
         logger_name
             Logger name with which the logger is instantiated.
+        level
+            Requested level of the logger
 
     Returns:
-        train_args
-            Updated training arguments for training model.
         train_logger
             Logger with updated effective log level
+        level
+            Level of the logger initialized
     """
 
     # Clear any existing handlers if necessary
@@ -39,26 +48,58 @@ def set_log_level(train_args, logger_name=None):
 
     # Configure Python native logger and transformers log level
     # If CLI arg is passed, assign same log level to python native logger
-    log_level = "WARNING"
-    if train_args.log_level != "passive":
-        log_level = train_args.log_level
-
-    # If CLI arg not is passed and env var LOG_LEVEL is set,
-    # assign same log level to both logger
+    lowest_log_level = DEFAULT_LOG_LEVEL_MAIN
+    if level != "passive":
+        lowest_log_level = level
     elif os.environ.get("LOG_LEVEL"):
-        log_level = os.environ.get("LOG_LEVEL")
-        train_args.log_level = (
-            log_level.lower()
+        # If CLI arg not is passed and env var LOG_LEVEL is set,
+        # assign same log level to both logger
+        lowest_log_level = (
+            os.environ.get("LOG_LEVEL").lower()
             if not os.environ.get("TRANSFORMERS_VERBOSITY")
             else os.environ.get("TRANSFORMERS_VERBOSITY")
         )
 
+    state = PartialState()
+    rank = state.process_index
+
+    log_on_all = os.environ.get("LOG_ON_ALL_PROCESSES")
+    if log_on_all:
+        log_level = lowest_log_level or DEFAULT_LOG_LEVEL_MAIN
+    else:
+        if state.is_local_main_process:
+            log_level = lowest_log_level or DEFAULT_LOG_LEVEL_MAIN
+            datasets.utils.logging.set_verbosity_warning()
+            transformers.utils.logging.set_verbosity_info()
+        else:
+            log_level = DEFAULT_LOG_LEVEL_WORKERS
+            datasets.utils.logging.set_verbosity_error()
+            transformers.utils.logging.set_verbosity_error()
+
+    log_format = f"Rank-{rank} [%(levelname)s]:%(filename)s:%(funcName)s: %(message)s"
+
     logging.basicConfig(
-        format="%(levelname)s:%(filename)s:%(message)s", level=log_level.upper()
+        format=log_format,
+        level=log_level.upper(),
     )
 
     if logger_name:
         train_logger = logging.getLogger(logger_name)
     else:
         train_logger = logging.getLogger()
-    return train_args, train_logger
+
+    return train_logger, log_level.lower()
+
+
+def pretty_print_args(args: Dict):
+    dump = "\n========================= Flat Arguments =========================\n"
+    for name, arg in args.items():
+        if arg:
+            dump += f"---------------------------- {name} -----------------------\n"
+            if hasattr(arg, "__dict__"):
+                arg = vars(arg)
+            max_len = max(len(k) for k in arg.keys())
+            for k, v in sorted(arg.items()):
+                dump += f"  {k:<{max_len}} : {v}\n"
+    dump += "========================= Arguments Done =========================\n"
+    return dump
