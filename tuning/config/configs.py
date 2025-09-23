132d00d9b6705d10e0a63619346f6c70236e4918
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-09-23 22:21:34 +0530

    feat: add odm plugin
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- tuning/config/configs.py
+++ tuning/config/configs.py
@@ -179,6 +179,7 @@ class DataArguments:
                     Used in conjunction with --do_dataprocessing_only"
         },
     )
+    is_odm: bool = False
 
     def __post_init__(self):
         def unescape(s):
