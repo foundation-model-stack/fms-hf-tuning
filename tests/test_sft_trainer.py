931c087dce2c9ecdd10af103456c9f32306e2bb8
romit <romit@ibm.com>
2025-11-07 07:30:02 +0000

    Added deppcopy for peft config
    Signed-off-by: romit <romit@ibm.com>

--- tests/test_sft_trainer.py
+++ tests/test_sft_trainer.py
@@ -633,7 +633,7 @@ def test_run_causallm_lora_invalid_train_params(param_name, param_val, exc_msg):
         setattr(invalid_params, param_name, param_val)
 
         with pytest.raises(ValueError, match=exc_msg):
-            sft_trainer.train(MODEL_ARGS, DATA_ARGS, invalid_params, PEFT_LORA_ARGS)
+            sft_trainer.train(MODEL_ARGS, DATA_ARGS, invalid_params, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 @pytest.mark.parametrize(
@@ -649,7 +649,7 @@ def test_run_causallm_lora_with_validation(dataset_path):
         data_args = copy.deepcopy(DATA_ARGS)
         data_args.validation_data_path = dataset_path
 
-        sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
         _validate_training(tempdir, check_eval=True)
 
 
@@ -670,7 +670,7 @@ def test_run_causallm_lora_with_validation_data_formatting(dataset_path):
             "### Text: {{element['Tweet text']}} \n\n### Label: {{text_label}}"
         )
 
-        sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
         _validate_training(tempdir, check_eval=True)
 
 
@@ -828,6 +828,8 @@ def test_successful_lora_target_modules_default_from_main():
             "q_proj",
             "v_proj",
         }, "target_modules are not set to the default values."
+
+        os.environ.pop("SFT_TRAINER_CONFIG_JSON_ENV_VAR", None)
 
 
 def test_run_causallm_lora_add_special_tokens():
@@ -1942,7 +1944,7 @@ def test_tokenizer_has_no_eos_token():
         # If we handled this badly, we would probably get something like a
         # TypeError: can only concatenate str (not "NoneType") to str error
         # when we go to apply the data formatter.
-        sft_trainer.train(model_args, DATA_ARGS, train_args, PEFT_LORA_ARGS)
+        sft_trainer.train(model_args, DATA_ARGS, train_args, copy.deepcopy(PEFT_LORA_ARGS))
         _validate_training(tempdir)
 
 
@@ -1954,7 +1956,7 @@ def test_invalid_dataset_text_field():
     data_args.dataset_text_field = "not found"
 
     with pytest.raises(KeyError):
-        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 ### Tests that giving dataset_text_field as well as formatter template gives error
@@ -1966,7 +1968,7 @@ def test_invalid_dataset_text_field_and_formatter_template():
     )
 
     with pytest.raises(ValueError):
-        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 ### Tests passing formatter with invalid keys gives error
@@ -1978,7 +1980,7 @@ def test_invalid_formatter_template():
     )
 
     with pytest.raises(KeyError):
-        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 ### Tests for bad training data (i.e., data_path is an unhappy value or points to an unhappy thing)
@@ -1988,7 +1990,7 @@ def test_malformatted_data():
     data_args.training_data_path = MALFORMATTED_DATA
 
     with pytest.raises((DatasetGenerationError, ValueError)):
-        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 def test_empty_data():
@@ -1997,7 +1999,7 @@ def test_empty_data():
     data_args.training_data_path = EMPTY_DATA
 
     with pytest.raises((DatasetGenerationError, ValueError)):
-        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, TRAIN_ARGS, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 ### Tests for bad tuning module configurations
@@ -2026,7 +2028,7 @@ def test_no_packing_needs_dataset_text_field_or_data_formatter_template():
         data_args.data_formatter_template = None
 
         with pytest.raises(ValueError):
-            sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+            sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 # TODO: Fix this case
@@ -2040,7 +2042,7 @@ def test_no_packing_needs_reponse_template():
         data_args.response_template = None
 
         with pytest.raises(ValueError):
-            sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+            sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 ### Tests for model dtype edge cases
@@ -2057,7 +2059,7 @@ def test_bf16_still_tunes_if_unsupported():
         model_args = copy.deepcopy(MODEL_ARGS)
         model_args.torch_dtype = "bfloat16"
 
-        sft_trainer.train(model_args, DATA_ARGS, train_args, PEFT_LORA_ARGS)
+        sft_trainer.train(model_args, DATA_ARGS, train_args, copy.deepcopy(PEFT_LORA_ARGS))
         _validate_training(tempdir)
 
 
@@ -2070,7 +2072,7 @@ def test_bad_torch_dtype():
         model_args.torch_dtype = "not a type"
 
         with pytest.raises(ValueError):
-            sft_trainer.train(model_args, DATA_ARGS, train_args, PEFT_LORA_ARGS)
+            sft_trainer.train(model_args, DATA_ARGS, train_args, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 def test_run_with_additional_callbacks():
@@ -2084,7 +2086,7 @@ def test_run_with_additional_callbacks():
             MODEL_ARGS,
             DATA_ARGS,
             train_args,
-            PEFT_LORA_ARGS,
+            copy.deepcopy(PEFT_LORA_ARGS),
             additional_callbacks=[TrainerCallback()],
         )
 
@@ -2103,7 +2105,7 @@ def test_run_with_bad_additional_callbacks():
                 MODEL_ARGS,
                 DATA_ARGS,
                 train_args,
-                PEFT_LORA_ARGS,
+                copy.deepcopy(PEFT_LORA_ARGS),
                 additional_callbacks=["NotSupposedToBeHere"],
             )
 
@@ -2124,7 +2126,7 @@ def test_run_with_bad_experimental_metadata():
                 MODEL_ARGS,
                 DATA_ARGS,
                 train_args,
-                PEFT_LORA_ARGS,
+                copy.deepcopy(PEFT_LORA_ARGS),
                 additional_callbacks=[TrainerCallback()],
                 exp_metadata=metadata,
             )
@@ -2143,7 +2145,7 @@ def test_run_with_good_experimental_metadata():
             MODEL_ARGS,
             DATA_ARGS,
             train_args,
-            PEFT_LORA_ARGS,
+            copy.deepcopy(PEFT_LORA_ARGS),
             additional_callbacks=[TrainerCallback()],
             exp_metadata=metadata,
         )
@@ -2166,7 +2168,7 @@ def test_pretokenized_dataset(dataset_path):
         data_args.dataset_text_field = None
         data_args.response_template = None
         data_args.training_data_path = dataset_path
-        sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+        sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
         _validate_training(tempdir)
 
 
@@ -2190,7 +2192,7 @@ def test_pretokenized_dataset_bad_args(dataset_text_field, response_template):
         # We should raise an error since we should not have a dataset text
         # field or a response template if we have pretokenized data
         with pytest.raises(ValueError):
-            sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+            sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 def test_pretokenized_dataset_wrong_format():
@@ -2208,7 +2210,7 @@ def test_pretokenized_dataset_wrong_format():
         # need to directly add validation prior to the dataset generation since datasets
         # is essentially swallowing a KeyError here.
         with pytest.raises(ValueError):
-            sft_trainer.train(MODEL_ARGS, data_args, train_args, PEFT_LORA_ARGS)
+            sft_trainer.train(MODEL_ARGS, data_args, train_args, copy.deepcopy(PEFT_LORA_ARGS))
 
 
 ###########################################################################
@@ -2241,7 +2243,7 @@ def test_run_with_bad_additional_data_handlers(additional_handlers):
                 MODEL_ARGS,
                 DATA_ARGS,
                 train_args,
-                PEFT_LORA_ARGS,
+                copy.deepcopy(PEFT_LORA_ARGS),
                 additional_data_handlers=additional_handlers,
             )
 
@@ -2256,7 +2258,7 @@ def test_run_with_additional_data_handlers_as_none():
             MODEL_ARGS,
             DATA_ARGS,
             train_args,
-            PEFT_LORA_ARGS,
+            copy.deepcopy(PEFT_LORA_ARGS),
             additional_data_handlers=None,
         )
         _validate_training(tempdir)
@@ -2303,7 +2305,7 @@ def test_run_by_passing_additional_data_handlers():
             MODEL_ARGS,
             DATA_ARGS,
             train_args,
-            PEFT_LORA_ARGS,
+            copy.deepcopy(PEFT_LORA_ARGS),
             additional_data_handlers={
                 TEST_HANDLER: DataHandler(
                     op=test_handler,
